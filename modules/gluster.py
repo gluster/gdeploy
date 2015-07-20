c222d9952312f666667923dc8b40ed04ac304b36
Nandaja Varma <nandaja.varma@gmail.com>
2015-07-20 15:25:58 +0530

    Updating the documentation in gluster.py

--- modules/gluster.py
+++ modules/gluster.py
@@ -17,6 +17,116 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+DOCUMENTATION = '''
+---
+module: gluster
+short_description: Implementaion of GlusterFS
+description:
+    - A module to set-up a GlusterFS cluster by creating a TSP and
+      creating volumes out of bricks. The host group on which
+      playbooks will be written for this module should contain
+      only one hostname which should be a part of the cluster
+      and from which all the Gluster commands will be executed.
+      This will make sure the commands are only run once.
+
+
+options:
+    commands:
+        required: True
+        choices: [peer, volume]
+        description: Specifies if the operation is a peer operation or
+                     volume operation. A peer operation does peer probe
+                     and peer detach, whereas a volume operation does
+                     volume create, delete, start, stop, add-brick and
+                     remove-brick.
+    action:
+        required: True
+        choices: [probe, detach] (If the command is peer)
+                 [create, delete, start, stop, add-brick, remove-brick, set] (If
+                 the command is volume)
+        description: Specifies what exact action is to be done. Under peer
+                     command, it can do probing and detaching. For volume
+                     command, this can be create, delete, start, stop,
+                     add-brick or remove-brick.
+    volume:
+        required: True (If command is volume)
+        description: Specifies the name of the Gluster volume to be created.
+
+    hosts:
+        required: True (If command is peer)
+        description: The list of all the hosts that are to be a part of the
+                     cluster.
+
+    bricks:
+        required: True (If command is volume and action is in [create,
+                  add-brick, remove-brick])
+        description: Specifies the bricks that constitutes the volume if
+                     the action is volume create, the new bricks to be
+                     added to a volume if the action is volume add-brick
+                     and the bricks to be removed out of a volume
+                     if the action is volume remove-brick.
+    force:
+        required: False
+        description: Specifies if an operation is to be forced.
+                     Applicable only to peer detach, volume { create, start,
+                     stop, add-brick}
+
+    key:
+        required: True if command is volume and  action is set
+        description: Set action sets options for the volume. Key
+                     specifies which option is to be set
+
+    value:
+        required: True if command is volume and action is set
+        description: Specifies the value to be set for the option
+                     specified bt key.
+
+    transport:
+        required: False
+        description: Specifies which transport type is to be used
+                     while creating the volume. Default is tcp.
+
+    replica:
+        required: False
+        description: Specifies the volume to be created is of type
+                     replicate or not. Can also be used to change the
+                     type while adding or removing bricks
+
+    replica_count:
+        required: True if replica is yes
+        description: Specifies the replica count if the volume type is
+                     to be replica
+
+    arbiter_count:
+
+
+
+
+    disperse:
+
+
+
+
+    disperse_count:
+
+
+
+
+
+    redundancy_count:
+
+
+
+    state:
+        required: True if action is remove-brick for volume command
+        choices: [start, stop, status, commit, force]
+        description: Specifies the state of the volume if one or more
+                     bricks are to be removed from the volume
+
+
+'''
+
+
 import sys
 import re
 from ansible.module_utils.basic import *
@@ -41,14 +151,12 @@ class Gluster(object):
         return value
 
     def get_host_names(self):
-        '''current_host(the one in which operation is running right now) and
-           hosts can be defined in the yml like this
-           current_host={{inventory_hostname}}
+        '''
+           Something like this can be used in the playbook:
            hosts="{% for host in groups[<group name>] %}
            {{ hostvars[host]['private_ip'] }},{% endfor %}"
 
         '''
-        self.current_host = self._validated_params('current_host')
         self.hosts = literal_eval(self._validated_params('hosts'))
 
     def append_host_name(self):
@@ -72,9 +180,7 @@ class Gluster(object):
     def gluster_peer_ops(self):
         if self.action in ['probe', 'detach']:
             self.get_host_names()
-            peers = [peer for peer in self.hosts
-                    if peer not in self.current_host]
-            for hostname in peers:
+            for hostname in self.hosts:
                 rc, output, err = self.call_gluster_cmd('peer',
                         self.action, self.force, hostname)
         else:
