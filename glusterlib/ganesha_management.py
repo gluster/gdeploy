472e51fa76774443a76f3aef8dd6a2548e8f3b6e
Nandaja Varma <nandaja.varma@gmail.com>
2015-11-23 15:01:23 +0530

    Fixing nfs-ganesha pcs auth issue

--- glusterlib/ganesha_management.py
+++ glusterlib/ganesha_management.py
@@ -43,10 +43,13 @@ class GaneshaManagement(YamlWriter):
             return
         del self.section_dict['action']
         self.section_dict = self.fix_format_of_values_in_config(self.section_dict)
+        self.get_cluster_nodes()
         action_func = { 'create-cluster': self.create_cluster,
                         'destroy-cluster': self.destroy_cluster,
                         'export-volume': self.export_volume,
-                        'unexport-volume': self.unexport_volume
+                        'unexport-volume': self.unexport_volume,
+                        'add-node': self.add_node,
+                        'delete-node': self.delete_node
                       }[action]
         if not action_func:
             print "\nError: Unknown action provided for nfs-ganesha. Exiting!\n"\
@@ -55,8 +58,38 @@ class GaneshaManagement(YamlWriter):
             self.cleanup_and_quit()
         action_func()
         self.filename = Global.group_file
+        msg = "NFS-Ganesha management(action: %s) triggered" % action
+        print "\nINFO: " + msg
+        Global.logger.info(msg)
         self.create_yaml_dict('base_dir', Global.base_dir, False)
+        self.create_yaml_dict('ha_base_dir', '/etc/ganesha', False)
         self.iterate_dicts_and_yaml_write(self.section_dict)
+
+
+    def get_cluster_nodes(self):
+        cluster = []
+        self.check_for_param_presence('cluster-nodes', self.section_dict)
+        cluster_nodes = self.section_dict.get('cluster-nodes')
+        self.cluster = self.pattern_stripping(cluster_nodes)
+        if Global.hosts:
+            if not set(self.cluster).issubset(set(Global.hosts)):
+                print "\nError: 'cluster_nodes' for nfs-ganesha should be " \
+                       "subset of the 'hosts'. Exiting!"
+                self.cleanup_and_quit()
+        else:
+            Global.hosts = self.cluster
+
+        self.write_config('cluster_nodes', self.cluster, Global.inventory)
+        self.write_config('master_node', [self.cluster[0]], Global.inventory)
+        self.section_dict['cluster_hosts'] = ','.join(node for node in
+                self.cluster)
+        self.section_dict['master_node'] = self.cluster[0]
+        try:
+            self.section_dict['pcs_cluster'] = ' '.join(self.cluster[1:])
+        except:
+            msg = "More nodes are needed to create a cluster."
+            print "\nError: " + msg
+            Global.logger.error(msg)
 
 
     def create_cluster(self):
@@ -65,29 +98,14 @@ class GaneshaManagement(YamlWriter):
         else:
             self.section_dict['ha_name'] = self.section_dict.pop('ha-name')
 
-        cluster = []
-        self.check_for_param_presence('cluster-nodes', self.section_dict)
-        cluster_nodes = self.section_dict.get('cluster-nodes')
-        cluster = self.pattern_stripping(cluster_nodes)
-        if Global.hosts:
-            if not set(cluster).issubset(set(Global.hosts)):
-                print "\nError: 'cluster_nodes' for nfs-ganesha should be " \
-                       "subset of the 'hosts'. Exiting!"
-                self.cleanup_and_quit()
-        else:
-            Global.hosts = cluster
+        self.get_host_vips(self.cluster)
 
-        self.write_config('cluster_nodes', cluster, Global.inventory)
-        self.write_config('master_node', [cluster[0]], Global.inventory)
-        self.section_dict['cluster_hosts'] = ','.join(node for node in cluster)
-        self.section_dict['master_node'] = cluster[0]
-
-        self.get_host_vips(cluster)
-
+        if not self.section_dict.get('volname'
+                ) and not self.present_in_yaml(Global.group_file, 'volname'):
+            print "'volname' not provided. Exiting!"
+            self.cleanup_and_quit()
         Global.playbooks.append('bootstrap-nfs-ganesha.yml')
-        if self.section_dict.get('volname'
-                ) or self.present_in_yaml(Global.group_file, 'volname'):
-            self.export_volume()
+        self.export_volume()
 
 
     def get_host_vips(self, cluster):
@@ -107,20 +125,40 @@ class GaneshaManagement(YamlWriter):
 
     def export_volume(self):
         if not self.section_dict.get('volname'
-                ) or not self.present_in_yaml(Global.group_file, 'volname'):
+                ) and not self.present_in_yaml(Global.group_file, 'volname'):
             print "'volname' not provided. Exiting!"
             self.cleanup_and_quit()
-        self.check_for_param_presence('cluster-nodes', self.section_dict)
-        Global.playbooks.append('ganesha-volume-configs.yml')
-        Global.playbooks.append('gluster-shared-volume-mount.yml')
         self.section_dict['value'] = "on"
+        Global.playbooks.append('ganesha-volume-configs.yml')
         Global.playbooks.append('gluster-volume-export-ganesha.yml')
 
     def destroy_cluster(self):
-        self.check_for_param_presence('cluster-nodes', self.section_dict)
         Global.playbooks.append('enable-nfs-ganesha.yml')
 
     def unexport_volume(self):
+        if not self.section_dict.get('volname'
+                ) and not self.present_in_yaml(Global.group_file, 'volname'):
+            print "'volname' not provided. Exiting!"
+            self.cleanup_and_quit()
         self.section_dict['value'] = "off"
         Global.playbooks.append('gluster-volume-export-ganesha.yml')
         return
+
+    def add_node(self):
+        self.check_for_param_presence('nodes', self.section_dict)
+        new_nodes = self.section_dict.get('nodes')
+        nodes = self.pattern_stripping(new_nodes)
+        self.section_dict['nodes'] = nodes
+        self.get_host_vips(nodes)
+        for node, vip in zip(nodes, self.section_dict['vips']):
+            nodes_list = {}
+            nodes_list['host'] = node
+            nodes_list['vip'] = vip
+            data.append(nodes_list)
+        self.create_yaml_dict('nodes_list', data, True)
+
+    def delete_node(self):
+        self.check_for_param_presence('nodes', self.section_dict)
+        new_nodes = self.section_dict.get('nodes')
+        nodes = self.pattern_stripping(new_nodes)
+        self.section_dict['nodes'] = nodes
