7886c2f4c4fab9bbec230392207955b65fc46e96
Sachidananda Urs <sacchi@gmail.com>
2016-02-26 17:28:47 +0530

    Create directory .gdeploy/logs before autolog calls open(Global.log_file, "a").
    Running gdeploy without .gdeploy/logs causes Traceback.

--- gdeploylib/__init__.py
+++ gdeploylib/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from global_vars import Global
 from autolog import logfunction,logmethod,logclass
 from gdeploy_logging import log_event
-from global_vars import Global
 from helpers import Helpers
 from yaml_writer import YamlWriter
 from call_features import call_features
