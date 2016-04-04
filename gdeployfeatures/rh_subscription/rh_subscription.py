5cc58d7a6b0145713ef53feed670aa4249d9a01c
Nandaja Varma <nandaja.varma@gmail.com>
2016-04-04 09:56:38 +0530

    Fixing issue #55: Fixing the creation of empty repos

--- gdeployfeatures/rh_subscription/rh_subscription.py
+++ gdeployfeatures/rh_subscription/rh_subscription.py
@@ -3,7 +3,8 @@
 Add functions corresponding to each of the actions in the json file.
 The function should be named as follows <feature name>_<action_name>
 """
-from gdeploylib import defaults
+from gdeploylib import defaults, Helpers
+helpers = Helpers()
 
 def rh_subscription_register(section_dict):
     return register_and_subscribe(section_dict)
@@ -13,16 +14,21 @@ def rh_subscription_unregister(section_dict):
 
 def rh_subscription_enable_repos(section_dict):
     section_dict, ret = register_and_subscribe(section_dict)
+    print section_dict
     return section_dict, defaults.ENABLE_REPO
 
 def rh_subscription_disable_repos(section_dict):
-    section_dict['repos'] = section_dict.get('repos') or "\'*\'"
+    global helpers
+    repos = section_dict.get('repos')
+    section_dict['repos'] = helpers.listify(repos) if repos else "\'*\'"
     return section_dict, defaults.DISABLE_REPO
 
 def rh_subscription_attach_pool(section_dict):
     return register_and_subscribe(section_dict)
 
 def register_and_subscribe(section_dict):
-    section_dict['rhsm_repos'] = section_dict.get('repos') or []
+    global helpers
+    repos = section_dict.get('repos')
+    section_dict['rhsm_repos'] = helpers.listify(repos) if repos else []
     section_dict['attach'] = section_dict.get('auto-attach') or 'false'
     return section_dict, defaults.SUBS_MGMT
