889405e4966a22c224ca87a75148e5e82f12d69a
Nandaja Varma <nandaja.varma@gmail.com>
2016-04-07 14:30:50 +0530

    Adding action names for the kubectl module ops

--- gdeployfeatures/kubectl/kubectl.py
+++ gdeployfeatures/kubectl/kubectl.py
@@ -5,8 +5,20 @@ The function should be named as follows <feature name>_<action_name>
 """
 from gdeploylib import defaults
 
-def kubectl_action1(section_dict):
+def kubectl_create(section_dict):
     return section_dict, defaults.YML_NAME
 
-def kubectl_action1(section_dict):
+def kubectl_run(section_dict):
+    return section_dict, defaults.YML_NAME
+
+def kubectl_exec(section_dict):
+    return section_dict, defaults.YML_NAME
+
+def kubectl_get(section_dict):
+    return section_dict, defaults.YML_NAME
+
+def kubectl_delete(section_dict):
+    return section_dict, defaults.YML_NAME
+
+def kubectl_stop(section_dict):
     return section_dict, defaults.YML_NAME
