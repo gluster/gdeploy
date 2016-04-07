260cc4c4b3545ab7ed115e7d939cf70de9bfb206
Nandaja Varma <nandaja.varma@gmail.com>
2016-04-07 10:29:51 +0530

    Adding skeleton for new kubectl module

---
+++ gdeployfeatures/kubectl/kubectl.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+Add functions corresponding to each of the actions in the json file.
+The function should be named as follows <feature name>_<action_name>
+"""
+from gdeploylib import defaults
+
+def kubectl_action1(section_dict):
+    return section_dict, defaults.YML_NAME
+
+def kubectl_action1(section_dict):
+    return section_dict, defaults.YML_NAME
