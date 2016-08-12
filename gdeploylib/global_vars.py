df607620bb4853a428a8d2c1fc26b4b25517440f
Sachidananda Urs <sacchi@gmail.com>
2016-08-12 19:26:23 +0530

    Add option to exit gdeploy in case of errors
    Earlier, errors were ignored and gdeploy continued.
    But in some cases we need gdeploy to error out if some actions failed.

--- gdeploylib/global_vars.py
+++ gdeploylib/global_vars.py
@@ -50,7 +50,7 @@ class Global(object):
     group_file = os.path.join(group_vars_dir, 'all')
     playbooks_file = os.path.join(os.path.realpath(base_dir),
                         'ansible_playbooks.yml')
-
+    ignore_errors = 'yes'
 
 
     #HEKETI DATA
