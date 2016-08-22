1372c75bc453839b7f6d99e963a41f293ae84047
Sachidananda Urs <sacchi@gmail.com>
2016-08-22 17:15:31 +0530

    Remove smb_enable option from CTDB
    * We no longer need to enable samba within CTDB
    * Remove smb_username and password setup from within CTDB
    * umount the volume after SELinux labels are set
    * Remove the CTDB_* options, they are no longer set in conf

--- gdeployfeatures/ctdb/ctdb.py
+++ gdeployfeatures/ctdb/ctdb.py
@@ -26,20 +26,9 @@ def ctdb_disable(section_dict):
     return section_dict, defaults.CHKCONFIG
 
 def ctdb_setup(section_dict):
-    option = ''
-    pattern = '^CTDB_.*'
-    opt_matches = [opt for opt in list(section_dict) if
-            re.search(pattern, opt)]
-    for key in opt_matches:
-        # This is a hack to side step the AttributeError. Will replace this with
-        # much cleaner default settings for Samba, in later releases.
-        if section_dict[key] and str(section_dict[key]).lower() != 'none':
-            option += key + '=' + str(section_dict[key]) + '\n'
-    section_dict['options'] = option
     section_dict['nodes'] = '\n'.join(sorted(set(Global.hosts)))
     paddress = section_dict.get('public_address')
     ctdbnodes = section_dict.get('ctdb_nodes')
-    enable_samba = section_dict.get('enable_samba')
 
     if paddress:
         if not isinstance(paddress, list):
@@ -68,26 +57,8 @@ def ctdb_setup(section_dict):
     section_dict, enable_yml = ctdb_enable(section_dict)
     section_dict, start_yml = ctdb_start(section_dict)
 
-    # There are three combinations possible:
-    # a. Setup CTDB
-    # b. Setup CTDB and enable samba
-    # c. Enable samba on a ctdb node
-
-    # Scenario a. Setup CTDB
-    if paddress and str(enable_samba).upper() != "YES":
-        yaml_list = [defaults.CTDB_SETUP, defaults.VOLSTOP_YML,
-                     defaults.VOLUMESTART_YML, enable_yml, start_yml,
-                     defaults.SMBSRV_YML]
-
-    # Setup CTDB and enable Samba
-    if paddress and str(enable_samba).upper() == "YES":
-        yaml_list = [defaults.CTDB_SETUP, defaults.VOLSTOP_YML,
-                     defaults.VOLUMESTART_YML, enable_yml, start_yml,
-                     defaults.SMB_FOR_CTDB, defaults.SMBSRV_YML]
-
-    # Enable Samba on an existing CTDB setup
-    if not paddress and str(enable_samba).upper() == "YES":
-        yaml_list = [enable_yml, start_yml, defaults.SMB_FOR_CTDB,
-                     defaults.SMBSRV_YML]
+    yaml_list = [defaults.CTDB_SETUP, defaults.VOLSTOP_YML,
+                 defaults.VOLUMESTART_YML, enable_yml, start_yml,
+                 defaults.SMB_FOR_CTDB]
 
     return section_dict, yaml_list
