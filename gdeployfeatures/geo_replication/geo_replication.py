6d474650c9cea9e5c0e792e75fc513428a5a8df6
Devyani Kota <divs.passion.18@gmail.com>
2016-10-28 16:02:57 +0530

    Add logging for geo-replication module.

--- gdeployfeatures/geo_replication/geo_replication.py
+++ gdeployfeatures/geo_replication/geo_replication.py
@@ -10,6 +10,9 @@ helpers = Helpers()
 def geo_replication_create(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.PUBKEY_SHARE)
+        Global.logger.info("Executing %s."% defaults.GEOREP_CREATE)
     return section_dict, [defaults.PUBKEY_SHARE, defaults.GEOREP_CREATE]
 
 def geo_replication_secure_session(section_dict):
@@ -17,6 +20,12 @@ def geo_replication_secure_session(section_dict):
     section_dict['secure'] = 'yes'
     section_dict['user'] = 'geoaccount'
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_SS)
+        Global.logger.info("Executing %s."% defaults.PUBKEY_SHARE)
+        Global.logger.info("Executing %s."% defaults.GEOREP_CREATE)
+        Global.logger.info("Executing %s."% defaults.SET_PERM_KEYS)
+        Global.logger.info("Executing %s."% defaults.GEOREP_START)
     return section_dict, [defaults.GEOREP_SS,
             defaults.PUBKEY_SHARE, defaults.GEOREP_CREATE,
             defaults.SET_PERM_KEYS, defaults.GEOREP_START]
@@ -24,31 +33,44 @@ def geo_replication_secure_session(section_dict):
 def geo_replication_start(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_START)
     return section_dict, defaults.GEOREP_START
 
 def geo_replication_stop(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_STOP)
     return section_dict, defaults.GEOREP_STOP
 
 def geo_replication_pause(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_PAUSE)
     return section_dict, defaults.GEOREP_PAUSE
 
 def geo_replication_delete(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_STOP)
+        Global.logger.info("Executing %s."% defaults.GEOREP_DELETE)
     return section_dict, [defaults.GEOREP_STOP, defaults.GEOREP_DELETE]
 
 def geo_replication_resume(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_RESUME)
     return section_dict, defaults.GEOREP_RESUME
 
 def geo_replication_config(section_dict):
     section_dict = parse_georep_data(section_dict)
     populate_inventory(section_dict)
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_CONFIG)
     return section_dict, defaults.GEOREP_CONFIG
 
 def geo_replication_failover(section_dict):
@@ -65,6 +87,11 @@ def geo_replication_failover(section_dict):
     section_dict['op'] = 'recover'
     section_dict['checkpoint'] = 'now'
     Global.master = section_dict['slave']
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.VOLUMESET_YML)
+        Global.logger.info("Executing %s."% defaults.GEOREP_CREATE)
+        Global.logger.info("Executing %s."% defaults.GEOREP_CONFIG)
+        Global.logger.info("Executing %s."% defaults.GEOREP_START)
     return section_dict, [defaults.VOLUMESET_YML,
             defaults.GEOREP_CREATE, defaults.GEOREP_CONFIG,
             defaults.GEOREP_START]
@@ -76,6 +103,9 @@ def geo_replication_failback(section_dict):
     section_dict['volname'] = section_dict['slavevolname']
     section_dict['key'] = ['geo-replication.indexing', 'changelog']
     Global.master = section_dict['slave']
+    if Global.trace:
+        Global.logger.info("Executing %s."% defaults.GEOREP_CONFIG)
+        Global.logger.info("Executing %s."% defaults.GEOREP_FAILBACK)
     return section_dict, [defaults.GEOREP_CONFIG, defaults.GEOREP_FAILBACK]
 
 def parse_georep_data(section_dict):
