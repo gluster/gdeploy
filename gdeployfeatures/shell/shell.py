1c81131b2508915ed5ff8cef1b269959e5c0960f
Devyani Kota <divs.passion.18@gmail.com>
2016-11-15 16:08:39 +0530

    Add logging for shell module

--- gdeployfeatures/shell/shell.py
+++ gdeployfeatures/shell/shell.py
@@ -3,6 +3,9 @@ Add functions corresponding to each of the actions in the json file.
 The function should be named as follows <feature name>_<action_name>
 """
 from gdeploylib import defaults
+from gdeploylib import Global
 
 def shell_execute(section_dict):
+	if Global.trace:
+		Global.logging.info("Executing %s."%defaults.SHELL_YML)
     return section_dict, defaults.SHELL_YML
