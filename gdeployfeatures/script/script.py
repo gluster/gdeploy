749a5755fd3ced7f48c75940b817bbe63ef52d68
Sachidananda Urs <sacchi@gmail.com>
2016-11-19 21:32:16 +0530

    Fix a backtrace caused because of an import

--- gdeployfeatures/script/script.py
+++ gdeployfeatures/script/script.py
@@ -3,7 +3,7 @@ Add functions corresponding to each of the actions in the json file.
 The function should be named as follows <feature name>_<action_name>
 """
 from gdeploylib import defaults
-from gdeploylib import Helpers
+from gdeploylib import Helpers, Global
 
 def script_execute(section_dict):
     helpers = Helpers()
