2c09ae0c1b372db251771c40f68d2f4ceaa09efb
Sachidananda Urs <sacchi@gmail.com>
2016-11-22 17:17:23 +0530

    Do not create a new config line for every invocation

--- gdeploy_setup.sh
+++ gdeploy_setup.sh
@@ -2,14 +2,27 @@
 
 test `id -u` -ne 0  && echo "Only root can run setup." && exit 1
 
+update_init_file ()
+{
+    INIT_FILE="$1"
+
+    # Clean up the stale environment variables
+    sed -i '/^export ANSIBLE_LIBRARY=/'d $INIT_FILE
+    sed -i '/^export GDEPLOY_TEMPLATES=/'d $INIT_FILE
+
+    echo "export ANSIBLE_LIBRARY=$ANSIBLE_LIBRARY:'$DIR/modules/'" >>$INIT_FILE
+    echo "export GDEPLOY_TEMPLATES='$DIR'" >> $INIT_FILE
+}
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-  echo "export ANSIBLE_LIBRARY=$ANSIBLE_LIBRARY:'$DIR/modules/'" >> ~/.zshrc
-  echo "export GDEPLOY_TEMPLATES='$DIR'" >> ~/.zshrc
+    update_init_file "$HOME/.zshrc"
 elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-  echo "export ANSIBLE_LIBRARY=$ANSIBLE_LIBRARY:'$DIR/modules/'" >> ~/.bashrc
-  echo "export GDEPLOY_TEMPLATES='$DIR'" >> ~/.bashrc
+    update_init_file "$HOME/.bashrc"
 fi
 
 python $DIR/setup.py install
+echo; echo
+echo -n "Please run the command 'source $INIT_FILE'  to "
+echo "update the environment variables."
