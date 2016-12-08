ed281273e6afe864abab4eedf3a91ae8071066aa
Sachidananda Urs <sacchi@gmail.com>
2016-12-08 13:20:31 +0530

    Remove duplicate entries cropped up while resolving conflicts

--- gdeploy_setup.sh
+++ gdeploy_setup.sh
@@ -3,22 +3,6 @@
 test `id -u` -ne 0  && echo "Only root can run setup." && exit 1
 PLUGIN_DIR=/usr/lib/python2.7/site-packages/ansible/plugins/callback/
 
-PLUGIN_DIR=/usr/lib/python2.7/site-packages/ansible/plugins/callback/
-
-update_init_file ()
-{
-    INIT_FILE="$1"
-
-    # Clean up the stale environment variables
-    sed -i '/^export ANSIBLE_LIBRARY=/'d $INIT_FILE
-    sed -i '/^export GDEPLOY_TEMPLATES=/'d $INIT_FILE
-
-    echo "export ANSIBLE_LIBRARY=$ANSIBLE_LIBRARY:'$DIR/modules/'" >>$INIT_FILE
-    echo "export GDEPLOY_TEMPLATES='$DIR'" >> $INIT_FILE
-}
-
-PLUGIN_DIR=/usr/lib/python2.7/site-packages/ansible/plugins/callback/
-
 update_init_file ()
 {
     INIT_FILE="$1"
@@ -40,6 +24,7 @@ elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
 fi
 
 # Copy the plugins to ansible plugin directory
+# Will move this to spec file later on.
 cp -f $DIR/plugins/callback/gdeploy.py $PLUGIN_DIR
 
 python $DIR/setup.py install
