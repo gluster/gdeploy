84268ebd6349946e85b6a5d818f11687e5d4a1c3
Sachidananda Urs <sacchi@gmail.com>
2017-01-05 16:29:39 +0530

    Add ignore_*_errors feature to script and shell modules

--- gdeployfeatures/shell/shell.py
+++ gdeployfeatures/shell/shell.py
@@ -2,7 +2,8 @@
 Add functions corresponding to each of the actions in the json file.
 The function should be named as follows <feature name>_<action_name>
 """
-from gdeploylib import defaults
+from gdeploylib import defaults, Global
 
 def shell_execute(section_dict):
+    Global.ignore_errors = section_dict.get('ignore_shell_errors')
     return section_dict, defaults.SHELL_YML
