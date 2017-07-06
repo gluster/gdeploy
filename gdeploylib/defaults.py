a1447c4304de50744b672fbc7a7e30b1abc7a437
Sachidananda Urs <surs@redhat.com>
2017-07-06 11:54:51 +0530

    Add feature for creating Geo-replication sessions

--- gdeploylib/defaults.py
+++ gdeploylib/defaults.py
@@ -167,18 +167,19 @@ CTDB_SETUP = 'setup_ctdb.yml'
 
 # GEO-REP
 
-GEOREP_CREATE = 'georep-session-create.yml'
+GEOREP_SETUP_MASTER = 'georep-init-ssh-setup-master.yml'
+GEOREP_SETUP_SLAVE_USERGRP = 'georep-init-slaves-group-setup.yml'
+GEOREP_SETUP_MOUNTBROKER = 'georep-init-slave-mountbroker-setup.yml'
+GEOREP_SETUP_GLUSTERD_RESTART = 'georep-init-slaves-glusterd-restart.yml'
+GEOREP_SETUP_SLAVE_PEM = 'georep-init-slave-pem-setup.yml'
+GEOREP_SETUP_SESSION_CREATE = 'georep-init-session-create.yml'
 GEOREP_START = 'georep-session-start.yml'
 GEOREP_STOP = 'georep-session-stop.yml'
 GEOREP_DELETE = 'georep-session-delete.yml'
 GEOREP_PAUSE = 'georep-session-pause.yml'
 GEOREP_RESUME = 'georep-session-resume.yml'
 GEOREP_CONFIG = 'georep-session-config.yml'
-GEOREP_SS = 'georep-secure-session.yml'
-SET_PERM_KEYS = 'georep-set-pemkeys.yml'
 GEOREP_FAILBACK = 'georep-fail-back.yml'
-PUBKEY_SHARE = 'georep_common_public_key.yml'
-
 
 # NFS-GANESHA
 
